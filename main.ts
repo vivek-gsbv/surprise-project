627f98abeb629f12e9e37e4469ae539bd61e3633
vivek-gsbv <neemayadav1221@gmail.com>
2025-11-27 08:26:58 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
 })
